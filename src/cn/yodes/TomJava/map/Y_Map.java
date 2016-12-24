f1d4358470ba0ba8b19b6237c391b3b823a737db
Yodeser <yodeser@gmail.com>
2016-12-24 15:50:13 +0800

    add search key(s) by value in Y_Map

--- src/cn/yodes/TomJava/map/Y_Map.java
+++ src/cn/yodes/TomJava/map/Y_Map.java
@@ -2,6 +2,7 @@ package cn.yodes.TomJava.map;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -73,4 +74,29 @@ public class Y_Map {
 	     
 		return null;
 	}
+	
+    //根据value值获取到对应的一个key值
+    public static String getKey(HashMap<String,String> map,String value){
+        String key = null;
+        //Map,HashMap并没有实现Iteratable接口.不能用于增强for循环.
+        for(String getKey: map.keySet()){
+            if(map.get(getKey).equals(value)){
+                key = getKey;
+            }
+        }
+        return key;
+        //这个key肯定是最后一个满足该条件的key.
+    }
+    
+    //根据value值获取到对应的所有的key值
+    public static List<String> getKeyList(HashMap<String,String> map,String value){
+        List<String> keyList = new ArrayList<String>();
+        for(String getKey: map.keySet()){
+            if(map.get(getKey).equals(value)){
+                keyList.add(getKey);
+            }
+        }
+        return keyList;
+    }
+	
 }
