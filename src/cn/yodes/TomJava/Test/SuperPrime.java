0067fe7ed57c539142fd1938167ac8068e9104ae
Yodes Yang <yodeser@gmail.com>
2017-03-09 20:04:19 +0800

    add change

---
+++ src/cn/yodes/TomJava/Test/SuperPrime.java
@@ -0,0 +1,40 @@
+package cn.yodes.TomJava.Test;
+
+import java.util.Scanner;
+
+public class SuperPrime {
+	public static void start(long data){
+		long[] primes = findPrime(data);
+		boolean flag;
+		for(int i =0 ; (primes[i] != 0); i++){
+			for(int j = 2; ( Math.pow(primes[i], j) <= data ); j++){
+				if(Math.pow(primes[i], j) == data){
+					System.out.println(primes[i] + " "+j);
+					return ;
+				}
+			}
+		}
+		System.out.println("No");
+		return ;
+	}
+	
+	public static long[] findPrime(long maxData){
+		long[] result = new long[(int) (maxData/2)];
+		boolean flag;
+		int k = 0;
+		for(long i = 2; i < maxData; i++){
+			flag = true;
+			for(long j = 2; j <= (int)Math.sqrt(i); j++){
+				if( (i % j) == 0){	flag = false; 	break;	}
+			}
+			if(flag)	result[k++] = i;
+		}
+		return result;
+	}
+	public static void main(String[] args){
+		long testNumber;
+		Scanner sc = new Scanner(System.in);
+		testNumber = sc.nextInt();
+		start(testNumber);
+	}
+}
---
+++ src/cn/yodes/TomJava/Test/SuperPrime2.java
@@ -0,0 +1,33 @@
+package cn.yodes.TomJava.Test;
+
+import java.util.Scanner;
+
+public class SuperPrime2 {
+	public static void main(String[] args){
+		int data;
+		Scanner sc = new Scanner(System.in);
+		data = sc.nextInt();
+		
+		int[] primes = new int[data/2];
+		boolean flag;
+		int k = 0;
+		for(int i = 2; i < data; i++){
+			flag = true;
+			for(int j = 2; j <= (int)Math.sqrt(i); j++){
+				if( (i % j) == 0){	flag = false; 	break;	}
+			}
+			if(flag)	primes[k++] = i;
+		}
+		
+		for(int i =0 ; (primes[i] != 0); i++){
+			for(int j = 2; ( Math.pow(primes[i], j) <= data ); j++){
+				if(Math.pow(primes[i], j) == data){
+					System.out.println(primes[i] + " "+j);
+					return ;
+				}
+			}
+		}
+		System.out.println("No");
+		return ;
+	}
+}
